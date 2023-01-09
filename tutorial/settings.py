d6507f86998bd5dd95ac050218bc16394d06a5b3
Nick Iacullo <iacullo@usc.edu>
2023-01-09 10:44:11 -0800

    feat(ICTRAINING-1391): create snippets app
    [ICTRAINING-1411]

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'tutorial.quickstart',
+    'tutorial.snippets',
 ]
 
 MIDDLEWARE = [
