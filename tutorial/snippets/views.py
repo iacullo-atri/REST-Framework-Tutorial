8f08832185a701afcf06fc2fa343b9451971f2f0
Nick Iacullo <iacullo@usc.edu>
2023-01-10 17:26:18 -0800

    feat(ICTRAINING-1391): complete tutorial 4
    [ICTRAINING-1414]

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -1,12 +1,8 @@
-from django.http import Http404
-from rest_framework import status
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from rest_framework.views import APIView
-from rest_framework import mixins
-from rest_framework import generics
+from django.contrib.auth.models import User
+from rest_framework import generics, permissions
 from tutorial.snippets.models import Snippet
-from tutorial.snippets.serializers import SnippetSerializer
+from tutorial.snippets.permissions import IsOwnerOrReadOnly
+from tutorial.snippets.serializers import SnippetSerializer, UserSerializer
 
 
 class SnippetList(generics.ListCreateAPIView):
@@ -15,6 +11,10 @@ class SnippetList(generics.ListCreateAPIView):
     """
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
@@ -23,3 +23,14 @@ class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     """
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
